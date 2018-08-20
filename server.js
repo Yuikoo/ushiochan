1e23865bc9e645c6d6cd89689df4800b0b7b97b2
Yuu <35911999+Yuikoo@users.noreply.github.com>
2018-08-20 13:19:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const join = require('path');
+const path = require('path');
 const express = require('express')
 const app = express()
 
-const img = express.static(join(__dirname, './img/'));
+const img = express.static(path.join(__dirname, './img/'));
 app.use('/img', img);
 app.set('view engine', 'ejs')
 
